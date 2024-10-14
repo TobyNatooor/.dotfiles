c4028b23649bd3d58b3c0f1ef551679b1b3ce3f1
Toby <60399231+TobyNatooor@users.noreply.github.com>
2024-10-14 11:35:37 +0200

    added rofi config

--- create_links.sh
+++ create_links.sh
@@ -6,3 +6,5 @@ ln .config/nvim/init.lua ~/.config/nvim/init.lua
 ln .config/starship.toml ~/.config/starship.toml
 mkdir ~/.config/gammastep
 ln .config/gammastep/config.ini ~/.config/gammastep/config.ini
+mkdir ~/.config/rofi
+ln .config/rofi/config.rasi ~/.config/rofi/config.rasi
